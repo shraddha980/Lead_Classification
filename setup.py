e69098e4d74e5a1ee3e160d30c0f558a05e0625d
shraddha980 <shraddhawork45@gmail.com>
2023-03-29 10:07:25 +0530

    updated requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+def get_requirements():
+    pass
+
+setup(
+    name="lead_classification",
+    author="ineuron",
+    author_email="shraddhawork45@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
