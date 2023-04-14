9ef2f5171c4a3fac5a93a0998c35c1f65b321740
shraddha980 <shraddhawork45@gmail.com>
2023-04-15 00:49:18 +0530

    New Files

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
+
 from typing import List
 
 REQUIREMENT_FILE_NAME="requirements.txt"
@@ -8,16 +9,17 @@ def get_requirements()->List[str]:
     
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
-        requirement_list = [requirement_name.replace("\n"," ") for requirement_name in requirement_list]
-
+    requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+    
     if HYPHEN_E_DOT in requirement_list:
         requirement_list.remove(HYPHEN_E_DOT)
     return requirement_list
 
 setup(
-    name="lead_classification",
+    name="Bank_leads",
+    version="0.0.2",
     author="ineuron",
     author_email="shraddhawork45@gmail.com",
-    packages=find_packages(),
-    install_requires=get_requirements()
+    packages = find_packages(),
+    install_requires=get_requirements(),
 )
